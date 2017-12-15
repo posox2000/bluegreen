13f73098f26b79bdf6472cebdc2c719958540d2e
posox2000 <posox2000@gmail.com>
2017-12-15 13:40:16 -0500

    to the RED
    to the RED

--- image.php
+++ image.php
@@ -7,9 +7,9 @@ $green = imagecolorallocate($im, 0, 255, 0);
 $red = imagecolorallocate($im, 255, 0, 0);
 
 // Draw a filled rectangle
-imagefilledrectangle($im, 0, 0, 199, 199, $blue);
+//imagefilledrectangle($im, 0, 0, 199, 199, $blue);
 //imagefilledrectangle($im, 0, 0, 199, 199, $green);
-//imagefilledrectangle($im, 0, 0, 199, 199, $red);
+imagefilledrectangle($im, 0, 0, 199, 199, $red);
 
 // Save the image
 header('Content-Type: image/png');
